3a271df0d3bbf59c88ac7d5f77869d769cdbd067
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-21 20:24:25 -0500

    use getters. test routes. render pizzas to front end.

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,39 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
   },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  },
-  toppings: []
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
