42f84bf8f410b3ef271bdacb72fbc62dcee5cf9c
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-22 10:15:18 -0500

    add replies to comments, delete replies from comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,7 +2,6 @@ const { Comment, Pizza } = require('../models');
 
 const commentController = {
   addComment({ params, body }, res) {
-    console.log(body);
     Comment.create(body)
       .then(({ _id }) => {
         return Pizza.findOneAndUpdate(
@@ -18,6 +17,32 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
+      .catch((err) => res.json(err));
+  },
+
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => res.json(err));
   },
 
